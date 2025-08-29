71fc3b73b01293f4c72c79cbb3a3d73123d29074
dazeGreyhead <126675366+dazeGreyhead@users.noreply.github.com>
2025-08-29 20:12:01 +0545

    1 error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/URG-Portfolio"
+  base:"/URG-Portfolio",
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
